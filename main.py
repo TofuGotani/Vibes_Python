bfafd02c7a88738c31a75d231ff0c62f08526968
Sabaniki <mdrxb950@gmail.com>
2021-03-20 13:29:16 +0900

    created mini keylogger

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import keyboard
+import time
+ASCII_LETTERS = list('abcdefghijklmnopqrstuvwxyz')
+
 if __name__ == '__main__':
-    print("hello")
+    prev_cnt = cnt = 0
+    key = ""
+    print("start")
+    while True:
+        for c in ASCII_LETTERS:
+            if keyboard.is_pressed(c):
+                cnt += 1
+                key = c
+
+        if prev_cnt != cnt:
+            print(key)
+        prev_cnt = cnt
+        time.sleep(0.1)
